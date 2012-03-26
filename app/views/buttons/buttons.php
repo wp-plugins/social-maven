0651b10cab9fe60da024759e1b418bf0c85ef14b
supercleanse <supercleanse@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-26 19:14:09 +0000

    1.0.0 Release of Social Maven
    git-svn-id: https://plugins.svn.wordpress.org/social-maven/trunk@523982 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ app/views/buttons/buttons.php
@@ -0,0 +1,15 @@
+<?php if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');} ?>
+<div class="<?php echo esc_html($classes); ?>">
+  <?php
+  if(isset($soma_options->twitter) and $soma_options->twitter)
+    SomaButtonsHelper::twitter_button($url,$description,'',$twitter_layout,'','','',$count_url);
+  if(isset($soma_options->googleplus) and $soma_options->googleplus)
+    SomaButtonsHelper::googleplus_button($url,$googleplus_layout,$googleplus_annotation);
+  if(isset($soma_options->linkedin) and $soma_options->linkedin)
+    SomaButtonsHelper::linkedin_button($url,$linkedin_layout);
+  if(isset($soma_options->pinterest) and $soma_options->pinterest)
+    SomaButtonsHelper::pinterest_button($url,$description,$post_options->pinterest_image,$pinterest_layout);
+  if(isset($soma_options->facebook) and $soma_options->facebook)
+    SomaButtonsHelper::facebook_button($url,$facebook_layout);
+  ?>
+</div>
